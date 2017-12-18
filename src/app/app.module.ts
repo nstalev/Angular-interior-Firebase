6773b883f37e2a3f9e5eda7d0d1263fda8991e9d
nstalev <stalev_a@abv.bg>
2017-12-18 12:30:17 +0200

    Angular Material added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from "@angular/router";
 import { SharedModule } from './components/shared/shared.module'
 import {MainPagesModule } from './components/main/main-pages.module'
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+
+
 
 import { AppComponent } from './app.component';
 import { routes } from './app.routing';
@@ -17,7 +21,8 @@ import { routes } from './app.routing';
     BrowserModule,
     RouterModule.forRoot(routes),
     SharedModule,
-    MainPagesModule
+    MainPagesModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/main/main-pages.module.ts
+++ src/app/components/main/main-pages.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterModule } from "@angular/router";
+import {MatButtonModule} from '@angular/material/button';
+
 
 import { MainComponents } from './index';
 
@@ -10,7 +12,8 @@ import { MainComponents } from './index';
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    MatButtonModule
   ],
   exports: [
     ...MainComponents
