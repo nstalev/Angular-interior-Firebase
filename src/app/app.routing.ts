b2ba538df6eb94e4e381bd1f96bcb2b251feaa4c
nstalev <stalev_a@abv.bg>
2017-12-19 13:06:30 +0200

    about us - updated

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,7 +3,7 @@ import { HomeComponent } from './components/main/home/home.component';
 import { ContactsComponent } from './components/main/contacts/contacts.component';
 import { PageNotFoundComponent } from './components/main/page-not-found/page-not-found.component';
 import { CarListComponent } from './components/cars/car-list/car-list.component';
-import { ForUsComponent } from './components/main/for-us/for-us.component';
+import { AboutUsComponent } from './components/main/about-us/about-us.component';
 
 
 
@@ -11,7 +11,7 @@ import { ForUsComponent } from './components/main/for-us/for-us.component';
 export const routes : Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home',    component: HomeComponent },
-    { path: 'info',    component: ForUsComponent },
+    { path: 'info',    component: AboutUsComponent },
     { path: 'contacts', component: ContactsComponent },
     { path: 'cars', loadChildren: './components/cars/cars.module#CarsModule' },
     { path: '**', component: PageNotFoundComponent }
--- src/app/components/main/index.ts
+++ src/app/components/main/index.ts
@@ -1,11 +1,11 @@
 import { HomeComponent } from "./home/home.component";
 import { ContactsComponent } from "./contacts/contacts.component";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
-import { ForUsComponent } from "./for-us/for-us.component";
+import { AboutUsComponent } from "./about-us/about-us.component";
 
 export const MainComponents = [
     HomeComponent,
     ContactsComponent,
-    ForUsComponent,
+    AboutUsComponent,
     PageNotFoundComponent
 ]
