3d0fa07762ded5fcb8cf5b063c2e46bd6dc17d4a
nstalev <stalev_a@abv.bg>
2017-12-19 14:16:32 +0200

    design update

--- src/app/components/cars/car-details/car-details.component.ts
+++ src/app/components/cars/car-details/car-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CarService } from './../../../core/services/cars/car.service'
 import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from 'ngx-gallery';
 import { ActivatedRoute, Router } from "@angular/router";
+import { Ng4LoadingSpinnerService } from 'ng4-loading-spinner';
 
 @Component({
   selector: 'app-car-details',
@@ -21,15 +22,18 @@ export class CarDetailsComponent implements OnInit, OnDestroy {
   constructor(
     private carService: CarService,
     private routeSnap: ActivatedRoute,
+    private spinnerService: Ng4LoadingSpinnerService
   ) { }
 
   ngOnInit() {
 
     this.paramsId = this.routeSnap.snapshot.params["id"];
+    this.spinnerService.show();
     this.sub$ = this.carService.getCarById(this.paramsId).subscribe(data =>{
     this.currentCar = data;
     this.arrayOfKeys = Object.keys(data['list']).slice(1, data['list'].length);
       this.generateList(this.arrayOfKeys)
+      this.spinnerService.hide();
     })
 
 
