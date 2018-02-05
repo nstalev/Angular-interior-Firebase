1fd5b1848d5903499a05f9a29c468037d8de8573
nstalev <stalev_a@abv.bg>
2018-02-05 21:41:40 +0200

    deploy

--- server.js
+++ server.js
@@ -7,6 +7,4 @@ app.use(express.static(__dirname + '/dist'));
 
 app.listen(process.env.Port || 8080);
 
-app.get('/*', function(req, res){
-    res.sendfile(path.join(__dirname + '/dist/index.html'));
-})
+
